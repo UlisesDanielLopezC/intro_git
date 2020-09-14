9b9c4f0bd916b1727c47320f1eec5b0dae8a7a14
UlisesDanielLopezC <ulises.lopez5724@alumnos.udg.mx>
2020-09-14 12:01:21 -0500

    Capturamos nombre completo usando fgets

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 
 int main(){
 
+    char nombre[50];
+
     printf("Hola mundo.\n");
+
+    printf("Nombre: ");
+    fgets(nombre, sizeof(nombre), stdin);
+    printf("\nHola %s.",nombre);
 
     return 0;
 }
